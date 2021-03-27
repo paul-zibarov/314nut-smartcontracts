91b82f1f80ef53a7cc0335f80693f7e7068b65ff
Paul Zibarov <paul.zibarov@gmail.com>
2021-03-27 22:07:53 +0200

    Add burn implementation for index token. Add convert methods to PUSD. Change math operators to SafeMath functions.

--- PNIndex.sol
+++ PNIndex.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity =0.8.0;
 
 library TransferHelper {
     function safeApprove(address token, address to, uint value) internal {
@@ -21,17 +22,32 @@ library TransferHelper {
 }
 
 library SafeMath {
-    function add(uint x, uint y) internal pure returns (uint z) {
-        require((z = x + y) >= x, 'ds-math-add-overflow');
+  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    if (a == 0) {
+      return 0;
     }
+    c = a * b;
+    assert(c / a == b);
+    return c;
+  }
 
-    function sub(uint x, uint y) internal pure returns (uint z) {
-        require((z = x - y) <= x, 'ds-math-sub-underflow');
-    }
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    // uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return a / b;
+  }
 
-    function mul(uint x, uint y) internal pure returns (uint z) {
-        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
-    }
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+    c = a + b;
+    assert(c >= a);
+    return c;
+  }
 }
 
 library Math {
@@ -67,10 +83,24 @@ interface IERC20 {
 }
 
 interface IPair {
-    function getReserves() external view returns (uint256 reserve0, uint256 reserve1);
+    function getReserves() external view returns (uint256 reserve0, uint256 reserve1, uint256 timestamp);
 }
 
-contract TokenBase is IERC20 { 
+interface IRouter {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external view returns (uint[] memory amounts);
+    
+    function getAmountsOut(uint amountIn, address[] memory path) external view returns (uint[] memory amounts);
+}
+
+contract TokenBase is IERC20 {
+    using SafeMath for uint;
+    
     string private _name = "PNToken";
     string private _symbol = "PNT";
     uint8 private _decimals = 18;
@@ -79,6 +109,7 @@ contract TokenBase is IERC20 {
     mapping (address => mapping (address => uint256)) public _allowances;
     
     event Mint(address indexed owner, uint indexAmount, uint amount0, uint amount1);
+    event Burn(address indexed owner, uint indexAmount, uint amount0, uint amount1);
 
     function name() public view override returns (string memory) {
         return _name;
@@ -94,8 +125,8 @@ contract TokenBase is IERC20 {
 
     function transfer(address _to, uint256 _value) public override returns (bool success) {
         if (_balances[msg.sender] >= _value && _value > 0) {
-            _balances[msg.sender] -= _value;
-            _balances[_to] += _value;
+            _balances[msg.sender] = _balances[msg.sender].sub(_value);
+            _balances[_to] = _balances[_to].add(_value);
             emit Transfer(msg.sender, _to, _value);
             return true;
         } else { 
@@ -105,9 +136,9 @@ contract TokenBase is IERC20 {
 
     function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
         if (_balances[_from] >= _value && _allowances[_from][msg.sender] >= _value && _value > 0) {
-            _balances[_to] += _value;
-            _balances[_from] -= _value;
-            _allowances[_from][msg.sender] -= _value;
+            _balances[_to] = (_balances[_to]).add(_value);
+            _balances[_from] = _balances[_from].sub(_value);
+            _allowances[_from][msg.sender] = _allowances[_from][msg.sender].sub(_value);
             emit Transfer(_from, _to, _value);
             return true;
         } else {
@@ -134,25 +165,176 @@ contract TokenBase is IERC20 {
     }
 }
 
-contract Index is TokenBase {
-    
+contract PNIndex is TokenBase {
     using SafeMath for uint;
 
-    address public token0;
-    address public token1;
-    IPair public pair;
+    address immutable public token0;
+    address immutable public token1;
+    IPair immutable public pair;
+    IRouter immutable public router;
     
     constructor(
         address _token0, 
         address _token1,
-        address _pair
-        
+        address _pair,
+        address _router
     ) {
         token0 = _token0;
         token1 = _token1;
         pair = IPair(_pair);
+        router = IRouter(_router);
     }
-
+    
+    function mint(uint amount0, uint amount1) public returns (uint256 indexAmount, uint256 amount0S, uint256 amount1S) {
+        require(amount1 != 0, "Token1 amount is equal to zero.");
+        require(amount0 != 0, "Token0 amount is equal to zero.");
+        (uint256 amount0R, uint256 amount1R,) = pair.getReserves();
+        amount1S = (amount0.div(2)).mul(amount1R).div(amount0R);
+        
+        if(amount1S < amount1) {
+            amount0S = amount0;
+            indexAmount =  _mint(amount0, amount1S);
+        } else {
+            amount0S = (amount1.mul(2)).mul(amount0R).div(amount1R);
+            amount1S = amount1;
+            indexAmount =  _mint(amount0S, amount1);
+        }
+    }
+    
+    function mintAndConvert(uint amount, address convertToken) public returns (uint indexAmount) {
+        uint256 amount0 = amount.mul(2).div(3);
+        uint256 amount1 = amount.div(3);
+        address[] memory addresses = new address[](2);
+        addresses[0] = token0;
+        addresses[1] = convertToken;
+        uint256[] memory amounts0 = router.swapExactTokensForTokens(amount0, 0, addresses, msg.sender, block.timestamp + 3600);
+        addresses[0] = token1;
+        uint256[] memory amounts1 = router.swapExactTokensForTokens(amount1, 0, addresses, msg.sender, block.timestamp + 3600);
+        uint256 amount0S;
+        uint256 amount1S;
+        (indexAmount, amount0S, amount1S) = mint(amounts0[1], amounts1[1]);
+        
+        if(amount0S < amounts0[1]) {
+            TransferHelper.safeTransfer(token0, msg.sender, amounts0[1] - amount0S);
+        }
+        
+        if(amount1S < amounts1[1]) {
+            TransferHelper.safeTransfer(token1, msg.sender, amounts1[1] - amount1S);
+        }
+    }
+    
+    function burn(uint256 amount) public returns (uint256 amount0, uint256 amount1) {
+        require(amount != 0, "Index token amount is equal to zero.");
+        require(_balances[msg.sender] >= amount, "Not enough index token.");
+        (amount0, amount1) = getBurnAmount(amount);
+        TransferHelper.safeTransfer(token0, msg.sender, amount0);
+        TransferHelper.safeTransfer(token1, msg.sender, amount1);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+        emit Burn(msg.sender, amount, amount0, amount1);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+    
+    function safeBurn(uint256 amount) public returns (uint256 amount0, uint256 amount1) {
+        require(amount != 0, "Index token amount is equal to zero.");
+        require(_balances[msg.sender] >= amount, "Not enough index token.");
+        (amount0, amount1) = getBurnAmount(amount);
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+        uint256 avaliableIndexAmount0;
+        uint256 avaliableIndexAmount1;
+        uint256 avaliableIndexAmount;
+        uint256 balance0S;
+        uint256 balance1S;
+        
+        if(balance0 < amount0) {
+            (balance1S, avaliableIndexAmount0) = getAmountOut(amount0, true);
+            avaliableIndexAmount = avaliableIndexAmount0;
+        } else {
+            avaliableIndexAmount0 = amount;
+            balance0 = amount0;
+        }
+        
+        if(balance1 < amount1) {
+            (balance0S, avaliableIndexAmount1) = getAmountOut(amount1, true);
+            avaliableIndexAmount = avaliableIndexAmount1;
+        } else {
+            avaliableIndexAmount1 = amount;
+            balance1 = amount1;
+        }
+        
+        if(avaliableIndexAmount0 != avaliableIndexAmount1 ) {
+            if(avaliableIndexAmount0 > avaliableIndexAmount1) {
+                avaliableIndexAmount = avaliableIndexAmount1;
+                balance0 = balance0S;
+            } else {
+                avaliableIndexAmount = avaliableIndexAmount0;
+                balance1 = balance1S;
+            }
+        }
+        
+        TransferHelper.safeTransfer(token0, msg.sender, balance0);
+        TransferHelper.safeTransfer(token1, msg.sender, balance1);
+        _balances[msg.sender] = _balances[msg.sender].sub(avaliableIndexAmount);
+        _totalSupply = _totalSupply.sub(avaliableIndexAmount);
+        emit Burn(msg.sender, avaliableIndexAmount, balance0, balance1);
+        emit Transfer(msg.sender, address(0), avaliableIndexAmount);
+    }
+    
+    function burnAndConvert(uint amount, address convertToken) public returns (uint amountConverted) {
+        require(amount != 0, "Index token amount is equal to zero.");
+        require(_balances[msg.sender] >= amount, "Not enough index token.");
+        (uint256 amount0, uint256 amount1) = getBurnAmount(amount);
+        address[] memory addresses = new address[](2);
+        addresses[0] = token0;
+        addresses[1] = convertToken;
+        uint256[] memory amounts0 = router.swapExactTokensForTokens(amount0, 0, addresses, msg.sender, block.timestamp + 3600);
+        addresses[0] = token1;
+        uint256[] memory amounts1 = router.swapExactTokensForTokens(amount1, 0, addresses, msg.sender, block.timestamp + 3600);
+        amountConverted = amounts0[1].add(amounts1[1]);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+        emit Burn(msg.sender, amount, amount0, amount1);
+        emit Transfer(msg.sender, address(0), amount);
+    }
+    
+    function getBurnAmount(uint amount) public view returns (uint amount0, uint amount1) {
+        (uint256 amount0R, uint256 amount1R,) = pair.getReserves();
+        amount0 = amount.div(Math.sqrt(amount1R.div(amount0R.mul(2))));
+        amount1 = (amount0.div(2).mul(amount1R.div(amount0R)));
+    }
+    
+    function getAmountOut(uint amount, bool isToken0) public view returns (uint counterTokenAmount, uint indexAmount) {
+        (uint256 amount0R, uint256 amount1R,) = pair.getReserves();
+        
+        if(isToken0) {
+            counterTokenAmount = (amount.div(2)).mul(amount1R).div(amount0R);
+        } else {
+            counterTokenAmount = (amount.mul(2)).mul(amount0R).div(amount1R);
+        }
+        
+        indexAmount = Math.sqrt(SafeMath.mul(amount, counterTokenAmount));
+    }
+    
+    function getAmountOutConvert(uint amount, address convertToken) public view returns (uint indexAmount) {
+        uint256 amount0 = amount.mul(2).div(3);
+        address[] memory addresses = new address[](2);
+        addresses[0] = token0;
+        addresses[1] = convertToken;
+        uint256 amounts0 = router.getAmountsOut(amount0, addresses)[1];
+        (, indexAmount) = getAmountOut(amounts0, true);
+    }
+    
+    function getBurnAmountConvert(uint amount, address convertToken) public view returns (uint amountConverted) {
+        (uint256 amount0, uint256 amount1) = getBurnAmount(amount);
+        address[] memory addresses = new address[](2);
+        addresses[0] = token0;
+        addresses[1] = convertToken;
+        amountConverted = router.getAmountsOut(amount0, addresses)[1];
+        addresses[1] = token1;
+        amountConverted = amountConverted.add(router.getAmountsOut(amount1, addresses)[1]);
+    }
+    
     function _mint(uint256 amount0, uint256 amount1) private returns (uint256 indexAmount) {
         require(amount0 != 0 && amount1 != 0, "Adjusted token amount is equal to zero.");
         TransferHelper.safeTransferFrom(token0, msg.sender, address(this), amount0);
@@ -165,19 +347,5 @@ contract Index is TokenBase {
         emit Transfer(address(0), msg.sender, amount);
         
         return amount;
-    }
-    
-    function mint(uint amount0, uint amount1) public returns (uint256 indexAmount) {
-        require(amount1 != 0, "Token1 amount is equal to zero.");
-        require(amount0 != 0, "Token0 amount is equal to zero.");
-        (uint256 amount0R, uint256 amount1R) = pair.getReserves();
-        uint256 amount1S = amount0 / 2 * amount1R / amount0R;
-        
-        if(amount1S < amount1) {
-            return _mint(amount0, amount1S);
-        } else {
-            uint256 amount0S = amount1 * 2 * amount0R / amount1R;
-            return _mint(amount0S, amount1);
-        }
     }
 }
